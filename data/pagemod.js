b90371091e007a3de281e4c2f56e8c50f6cbb1e0
Jared Null <null.jared@gmail.com>
2015-07-24 00:14:31 -0700

    Added timeout example
    timeout is said to fix problems with html2canvas processing, but this
    didn't work for me.

--- data/pagemod.js
+++ data/pagemod.js
@@ -1,18 +1,34 @@
 // pagemod.js
 
-// * version 5.0 alpha 2 library (callback) current way.
+//timeout method from cptdanko in comment 7 https://github.com/niklasvh/html2canvas/issues/491
+//* This doesn't work for me -Null 7/23/2015
+function getScreenshot(callbackFunc){
+    var content = document.body;
+    setTimeout(function(){screenCap();},0); //tried changing 0 to 5000 but didn't result in a difference. 
+    function screenCap(){
+        html2canvas(content, {
+            onrendered: function(canvas) {
+                document.documentElement.replaceChild(canvas, document.body);
+            }
+        });
+    }
+}
+getScreenshot();
+//*/
+
+/* version 5.0 alpha 2 library (callback) current way.
 html2canvas(document.head, {
     taintTest:false,
     logging:true,
     allowTaint: true,
     useCORS: true,
     onrendered: function(canvas) {
-        document.documentElement.replaceChild(canvas, document.head);
+        document.documentElement.replaceChild(canvas, document.body);
     }
 });
 //*/
 
-/* This works for html2canvas 4.0 (no callback) deprecated way.
+/* html2canvas 4.0 (no callback - deprecated).
 html2canvas(document.body, {
     taintTest:false,
     logging:true,
